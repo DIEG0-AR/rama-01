a2e64614f921830d9aa66c0c94a1eb1006524022
jdiego <jdiegomatamtz@gmail.com>
2023-09-28 02:50:45 -0500

    suma de dos numeros

---
+++ suma.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+class main ()
+{
+ int a, b, c;
+
+ cout<<"suma de dos numeros"<<endl;
+ cout<<"agrega el primer numero"<<endl;
+ cin>>a
+ cout<<"agrega el segundo numero"<<endl;
+ cin>>b
+	 c=a+b;
+ cout<<"la suma es de:"<<c<<endl;
+}
